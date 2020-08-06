c4214badbdde8b3e549532951eef9d0ce3640f27
pilikiwi <pilikiwi@gmail.com>
2020-08-05 19:59:40 -0700

    3.19: Phonebook database, step7

--- models/person.js
+++ models/person.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
 const url = process.env.MONGODB_URI
 
 console.log(`connecting to :  ${url}`)
@@ -14,9 +17,11 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {type: String, required: true, unique: true},
+    number: {type: String, required: true},
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) =>{
